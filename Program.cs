2fa388645dbfac9e68b33d7909374f93a01ce246
Frédéric Jacques <gary.bob88@gmail.com>
2024-05-17 17:06:50 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// This projet i a demo on how to read bytes from a LoRa USB dongle
+// Using modern c#
+
+using System;
+
+Console.WriteLine("Hello, World!");
+
+try {
+    using (var serialPort = new System.IO.Ports.SerialPort("COM6", 115200)) {
+        Console.WriteLine("Opening port");
+        serialPort.Open();
+        Console.WriteLine("Port opened");
+        while (true) {
+            Console.WriteLine("Bytes to read: " + serialPort.BytesToRead);
+            if (serialPort.BytesToRead > 0) {
+                byte[] buffer = new byte[serialPort.BytesToRead];
+                serialPort.Read(buffer, 0, buffer.Length);
+                Console.WriteLine(BitConverter.ToString(buffer));
+            }
+            await Task.Delay(1000);
+        }
+    }
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
